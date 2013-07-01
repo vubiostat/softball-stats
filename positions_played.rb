289f2966fa6c0bfa4ec952c5117ac180ee3b6913
Will Gray <graywh@gmail.com>
2013-07-01 11:17:16 -0500

    Account for multiple positions played in same game

--- positions_played.rb
+++ positions_played.rb
@@ -8,11 +8,11 @@ options = OptionParser.new do |opts|
   opts.banner = "Usage: #{__FILE__} [options] [files]"
 
   opts.on('-n', '--player NAME', '--name NAME', 'Player name to search') do |n|
-    OPTS[:player] = n
+    OPTS[:player] = Regexp.new(n)
   end
 
   opts.on('-p', '--position POS', 'Position to search') do |p|
-    OPTS[:position] = p
+    OPTS[:position] = Regexp.new(p)
   end
 
   opts.on('-h', '--help') do
@@ -23,28 +23,19 @@ end
 
 options.parse!(ARGV)
 
-if OPTS[:player].nil? && OPTS[:position].nil?
-  puts options.inspect
-  exit
-elsif OPTS[:player].nil?
-  PATTERN = Regexp.new("\\b(\\w+)\\b [^ ]*\\b(?:#{OPTS[:position]})\\b")
-elsif OPTS[:position].nil?
-  PATTERN = Regexp.new("\\b(?:#{OPTS[:player]})\\b \\b(\\w+)\\b")
-else
-  PATTERN = Regexp.new("\\b(#{OPTS[:player]})\\b [^ ]*\\b(#{OPTS[:position]})\\b")
-end
-
-# puts OPTS.inspect
-# puts ARGV.inspect
-# puts PATTERN.inspect
-
 DB = Hash.new(0)
 
 ARGV.each do |file|
   File.open(file).each_line do |line|
-    line.split(' -> ').each do |sub_line|
-      if sub_line =~ PATTERN
-        DB[[$1,$2].join(':')] += 1
+    if line =~ /^ *[0-9]* ([^ ]+ [^ ]+.*)/
+      $1.split(' -> ').each do |sub_line|
+        name, positions = sub_line.split(' ')
+        positions.split('/').each do |position|
+          if (OPTS[:player].nil? || name =~ OPTS[:player]) &&
+            (OPTS[:position].nil? || position =~ OPTS[:position])
+            DB[[name,position].join(':')] += 1
+          end
+        end
       end
     end
   end
